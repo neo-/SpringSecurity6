799a2dadf558f5d39a3f72ff20a8b4f3112889c1
rajeevan <naveejr@gmail.com>
2023-04-07 20:04:33 +0530

    Endpoint Authorization

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.springsecurity.config;
 
+import com.example.springsecurity.config.security.SecurityAuthorities;
+import com.example.springsecurity.config.security.SecurityRole;
 import com.example.springsecurity.config.security.filters.ApiKeyFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -26,19 +29,38 @@ public class SecurityConfig {
 				.httpBasic()
 				.and()
 				.addFilterAt(new ApiKeyFilter(apiKey), BasicAuthenticationFilter.class)
-				.authorizeRequests().anyRequest().authenticated()
+				.authorizeRequests().anyRequest()
+				//.authenticated()
+				//.permitAll()
+				.hasAuthority(SecurityAuthorities.WRITE)
 				.and().build();
 	}
 
 	@Bean
 	public UserDetailsService userDetailsService() {
 		var uds = new InMemoryUserDetailsManager();
-		var u1 = User.withUsername("admin")
+		var u1 = User.withUsername("bill")
 				.password("12345")
-				.authorities("read")
+				.authorities(
+						new SimpleGrantedAuthority(SecurityAuthorities.READ),
+						new SimpleGrantedAuthority(SecurityAuthorities.WRITE),
+						new SimpleGrantedAuthority(SecurityRole.MANAGER)
+				)
+				.build();
+		var u2 = User.withUsername("jenifer")
+				.password("12345")
+				.authorities(SecurityAuthorities.READ)
+				//.roles(SecurityRole.MANAGER)
+				.build();
+		var u3 = User.withUsername("john")
+				.password("12345")
+				.authorities(SecurityAuthorities.READ)
+				//.roles(SecurityRole.USER)
 				.build();
 
 		uds.createUser(u1);
+		uds.createUser(u2);
+		uds.createUser(u3);
 		return uds;
 	}
 
---
+++ src/main/java/com/example/springsecurity/config/security/SecurityAuthorities.java
@@ -0,0 +1,6 @@
+package com.example.springsecurity.config.security;
+
+public class SecurityAuthorities {
+	public static final String READ = "READ";
+	public static final String WRITE = "WRITE";
+}
